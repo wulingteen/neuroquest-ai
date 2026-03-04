3c2347f569ca9b7da6fb84ed858ffb4bc7510c8a
zmx0210 <zmx2018057484@gmail.com>
2026-03-04 19:52:05 +0800

    feat: add cat

--- src/app/lab/_components/Profile3D.tsx
+++ src/app/lab/_components/Profile3D.tsx
@@ -4,113 +4,62 @@ import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Environment, Float, ContactShadows } from "@react-three/drei";
 import { useMemo } from "react";
 import * as THREE from "three";
+import catData from "./cat_data.json";
 
-function BarbapapaBody() {
-    const points = useMemo(() => {
-        const pts = [];
-        // Define the profile curve (x, y)
-        pts.push(new THREE.Vector2(0, 0));
-        pts.push(new THREE.Vector2(0.6, 0.05));
-        pts.push(new THREE.Vector2(1.1, 0.4));
-        pts.push(new THREE.Vector2(1.4, 1.0));
-        pts.push(new THREE.Vector2(1.35, 1.8));
-        pts.push(new THREE.Vector2(0.9, 2.4));
-        pts.push(new THREE.Vector2(0.6, 2.9)); // Narrow part/neck
-        pts.push(new THREE.Vector2(0.55, 3.4)); // Head start
-        pts.push(new THREE.Vector2(0.4, 3.9));
-        pts.push(new THREE.Vector2(0, 4.1)); // Top center
-        return pts;
+function ChonkyCat() {
+    const geometry = useMemo(() => {
+        const geo = new THREE.BufferGeometry();
+
+        const vertices = new Float32Array(catData.vertices);
+        const normals = new Float32Array(catData.normals);
+        const indices = new Uint32Array(catData.indices);
+
+        geo.setAttribute('position', new THREE.BufferAttribute(vertices, 3));
+        geo.setAttribute('normal', new THREE.BufferAttribute(normals, 3));
+        geo.setIndex(new THREE.BufferAttribute(indices, 1));
+
+        geo.computeBoundingBox();
+        const box = geo.boundingBox!;
+        const center = box.getCenter(new THREE.Vector3());
+
+        // Center the geometry
+        geo.translate(-center.x, -center.y, -center.z);
+
+        // Scale it to a nice size
+        geo.computeBoundingBox();
+        const size = geo.boundingBox!.getSize(new THREE.Vector3());
+        const maxDim = Math.max(size.x, size.y, size.z);
+        const scale = 4.0 / maxDim;
+        geo.scale(scale, scale, scale);
+
+        // Final bounding box for vertical alignment
+        geo.computeBoundingBox();
+        const finalBox = geo.boundingBox!;
+        geo.translate(0, -finalBox.min.y - 2.0, 0);
+
+        // Quick rotation adjustment
+        geo.rotateX(-Math.PI / 2); // Blender usually has Z-up, three.js has Y-up
+        geo.rotateY(-Math.PI / 8);
+        geo.rotateZ(Math.PI / 8);
+
+        return geo;
     }, []);
 
     return (
-        <mesh castShadow receiveShadow>
-            <latheGeometry args={[points, 64]} />
+        <mesh
+            geometry={geometry}
+            castShadow
+            receiveShadow
+        >
             <meshStandardMaterial
                 color="#A020F0"
-                roughness={0.2}
+                roughness={0.3}
                 metalness={0.1}
                 emissive="#330044"
                 emissiveIntensity={0.2}
+                flatShading={true} // Emphasize the low-poly look!
             />
         </mesh>
-    );
-}
-
-function Face() {
-    return (
-        <group position={[0, 3.4, 0.6]} rotation={[0, 0, 0]}>
-            {/* Eyes - Vertical Ovals with 3D depth */}
-            <group position={[-0.2, 0.2, 0]}>
-                <mesh scale={[0.7, 1.4, 0.5]}>
-                    <sphereGeometry args={[0.2, 32, 32]} />
-                    <meshStandardMaterial color="white" roughness={0.1} />
-                </mesh>
-                <group position={[0, 0, 0.08]} scale={[0.4, 1.1, 0.2]}>
-                    <mesh>
-                        <sphereGeometry args={[0.12, 32, 32]} />
-                        <meshStandardMaterial color="black" roughness={0.1} />
-                    </mesh>
-                </group>
-            </group>
-            <group position={[0.2, 0.2, 0]}>
-                <mesh scale={[0.7, 1.4, 0.5]}>
-                    <sphereGeometry args={[0.2, 32, 32]} />
-                    <meshStandardMaterial color="white" roughness={0.1} />
-                </mesh>
-                <group position={[0, 0, 0.08]} scale={[0.4, 1.1, 0.2]}>
-                    <mesh>
-                        <sphereGeometry args={[0.12, 32, 32]} />
-                        <meshStandardMaterial color="black" roughness={0.1} />
-                    </mesh>
-                </group>
-            </group>
-
-            {/* Simple Smile */}
-            <mesh position={[0, -0.15, -0.1]} rotation={[Math.PI / 1.7, 0, 0]}>
-                <torusGeometry args={[0.14, 0.015, 16, 32, Math.PI]} />
-                <meshStandardMaterial color="#222" />
-            </mesh>
-        </group>
-    );
-}
-
-function Limbs() {
-    return (
-        <group>
-            {/* Arms */}
-            <mesh position={[-1.3, 1.8, 0]} rotation={[0, 0, -Math.PI / 6]}>
-                <capsuleGeometry args={[0.18, 0.5, 16, 16]} />
-                <meshStandardMaterial color="#A020F0" roughness={0.2} />
-            </mesh>
-            <mesh position={[1.3, 1.8, 0]} rotation={[0, 0, Math.PI / 6]}>
-                <capsuleGeometry args={[0.18, 0.5, 16, 16]} />
-                <meshStandardMaterial color="#A020F0" roughness={0.2} />
-            </mesh>
-
-            {/* Feet */}
-            <group position={[-0.5, 0.15, 0]}>
-                <mesh rotation={[Math.PI / 2, 0, 0]}>
-                    <capsuleGeometry args={[0.25, 0.3, 16, 16]} />
-                    <meshStandardMaterial color="#A020F0" roughness={0.2} />
-                </mesh>
-            </group>
-            <group position={[0.5, 0.15, 0]}>
-                <mesh rotation={[Math.PI / 2, 0, 0]}>
-                    <capsuleGeometry args={[0.25, 0.3, 16, 16]} />
-                    <meshStandardMaterial color="#A020F0" roughness={0.2} />
-                </mesh>
-            </group>
-        </group>
-    );
-}
-
-function Character() {
-    return (
-        <group position={[0, -2.1, 0]}>
-            <BarbapapaBody />
-            <Face />
-            <Limbs />
-        </group>
     );
 }
 
@@ -134,7 +83,7 @@ export default function Profile3D() {
                 <pointLight position={[-10, -10, -10]} intensity={1} />
 
                 <Float speed={1.5} rotationIntensity={0.2} floatIntensity={0.3}>
-                    <Character />
+                    <ChonkyCat />
                 </Float>
 
                 <ContactShadows
